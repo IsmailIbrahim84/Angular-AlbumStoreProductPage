d65da7164397fb2716e07d2b1efc964ef7e72de0
IsmailIbrahim84 <beisso75@hotmail.com>
2021-01-24 14:40:10 +0200

    Adding a Type To The albumInfo Property

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {Http, Response} from '@angular/http';
 import 'rxjs/add/operator/map';
+import {Album} from './album';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable (  )
 
@@ -8,8 +10,7 @@ import 'rxjs/add/operator/map';
 export class ProductService {
  private _albumUrl = '../assets/album.json';
  constructor(private _http: Http) { }
-getAlbum(id: number) {
-    return this._http.get<Album>(this._albumUrl).map((response) => response.json());
+getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
 }
-
 }
