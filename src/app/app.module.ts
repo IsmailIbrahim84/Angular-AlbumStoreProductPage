bbcb526862c971c7540095f024a7b2d1fee64792
IsmailIbrahim84 <beisso75@hotmail.com>
2021-03-20 15:39:08 +0200

    Adding Routes In The NgModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,11 @@ import {ProductService} from './product.service';
 import {HttpModule} from '@angular/http';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
-import {RouterModule} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: 'products', component: ProductListComponent},{path: 'product/id', component: ProductPageComponent}
+];
 
 @NgModule({
   declarations: [
@@ -24,7 +28,7 @@ import {RouterModule} from '@angular/router';
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ProductService],
   bootstrap: [AppComponent]
